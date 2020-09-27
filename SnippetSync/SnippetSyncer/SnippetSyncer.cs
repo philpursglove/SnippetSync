707671b0357b1739c11d8c9585e4501d2abce72b
Phil Pursglove <philpursglove@users.noreply.github.com>
2020-09-27 20:31:10 +0100

    Added SnippetSyncer project

---
+++ SnippetSync/SnippetSyncer/SnippetSyncer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SnippetSyncer
+{
+    public class SnippetSyncer
+    {
+        // SyncSnippets
+
+        // SyncSnippetFolder(string folderPath, string RepoUrl)
+        // {
+        //      DateTime localFileTimestamp = GetTimeStampFromLocalFile(folderPath)
+        //      DateTime repoTimestamp = GetRepoLastUpdateTime(repoUrl)
+        //      
+        //      if repoTimestamp > localFIleTimeStamp
+        //      {
+        //          ClearFolder(folderPath)
+        //          List<GithubFile> repoFiles = GetFilesListFromRepo
+        //          foreach file in repoFiles
+        //          {
+        //              DownloadFIle(file)
+        //          }
+        //      }
+        // }
+
+        // GetFilesListFromRepo(repoUrl)
+
+        // DownloadFile(string fileDownloadUrl)
+
+        // SaveTimestampFile
+
+        // GetRepoLastUpdateTime(string repoUrl)
+
+        // GetTimestampFromLocalFile
+
+        // ClearFolder(string folderPath)
+    }
+}
