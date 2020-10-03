c45fc6302c9c611da188b6a8b91a4d3011fe5aba
Phil Pursglove <philpursglove@users.noreply.github.com>
2020-10-03 21:32:57 +0100

    Added Full test to bypass the Flurl test setup

---
+++ SnippetSync/SnippetSyncer.Tests/FullTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System.Linq;
+
+namespace SnippetSyncer.Tests
+{
+    [TestFixture]
+    public class FullTest
+    {
+        [Test]
+        public void End_To_End_Test()
+        {
+            SnippetSyncer syncer = new SnippetSyncer();
+            string path = @"C:\Temp\SnippetSync";
+            string url = "https://github.com/philpursglove/CSharpSnippets";
+
+            syncer.SyncSnippetFolder(path, url);
+
+            Assert.That(Directory.GetFiles(path).Count, Is.EqualTo(4));
+        }
+
+    }
+}
